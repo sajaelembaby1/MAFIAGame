e3f1ccf39850fab3acd40cce8320c5c2c9af5740
sajaelembaby1 <SE00177@tkh.edu.eg>
2024-05-02 20:00:33 +0300

    Interaction

---
+++ Assets/Script/NPC.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public abstract class NPC : MonoBehaviour, IInteractable
+{
+    [SerializeField] SpriteRenderer _interactSprite;
+
+    private Transform _playerTransform;
+    private const float INTERACT_DISTANCE = 2F;
+
+    private void Start()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame && IsWithinInteractDistance())
+        {
+            //interact
+            Interact();
+        }
+
+        if (_interactSprite.gameObject.activeSelf && !IsWithinInteractDistance())
+        {
+            // turn off the sprite
+            _interactSprite.gameObject.SetActive(false);
+        }
+
+        else if (!_interactSprite.gameObject.activeSelf && IsWithinInteractDistance())
+        {
+            //turn on the sprite
+            _interactSprite.gameObject.SetActive(true);
+        }
+
+    }
+
+
+    public abstract void Interact();
+
+    private bool IsWithinInteractDistance()
+    {
+        if (Vector2.Distance(_playerTransform.position, transform.position) < INTERACT_DISTANCE)
+        {
+            return true;
+        }
+        else
+        {
+
+            return false;
+        }
+    }
+}
